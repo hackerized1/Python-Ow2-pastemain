b9f6cb65f684919318db53825590398d0a187e8c
hackerized1 <50502173+hackerized1@users.noreply.github.com>
2022-12-06 05:56:29 +0300

    Python-Ow2-pastemain
    Python-Ow2-pastemain

---
+++ grabber.py
@@ -0,0 +1,171 @@
+
+import cv2
+import win32api, win32con, win32ui, win32gui
+import numpy as np
+import threading
+import time
+import random
+import dxcam
+from numba import jit
+
+
+import clr
+# change this to wherever your built DLL is
+clr.AddReference(' compiled dll location\\DLL\\ClassLibrary1\\ClassLibrary1.dll')
+from ClassLibrary1 import Class1
+ud_mouse = Class1()
+ud_mouse.Run_Me()
+
+
+#predict = Prediction()
+
+
+class Grabber:
+    def __init__(self, x_multiplier, y_multiplier, y_difference, flick_speed) -> None:
+        # self.lower = np.array([139, 96, 129], np.uint8)
+        # self.upper = np.array([169, 255, 255], np.uint8)
+        self.lower = np.array([139, 95, 154], np.uint8)
+        self.upper = np.array([153, 255, 255], np.uint8)
+        self.x_multiplier = x_multiplier         # multiplier on x-coordinate
+        self.y_multiplier = y_multiplier         # multiplier on y-coordinate
+        self.y_difference = y_difference         # the amount of pixels added to the y-coordinate (aims higher)
+        self.flick_speed = flick_speed
+
+    def build_title(self, length) -> str:
+        """return a randomly generated window title to prevent detections"""
+        chars = [
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#',
+            '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '/', '?'
+        ]
+        return ''.join(random.choice(chars) for character in range(length))
+ 
+    def find_dimensions(self, box_size): 
+        """Calculates constants required for the bot."""
+        self.box_size = box_size
+        self.box_middle = int(self.box_size / 2) 
+        self.y = int(((1080 / 2) - (self.box_size / 2))) 
+        self.x = int(((1920 / 2) - (self.box_size / 2))) 
+
+        
+
+    def process_frame(self, frame):
+        """Performs operations on a frame to improve contour detection."""
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        processed = cv2.inRange(hsv, self.lower, self.upper)
+
+        processed = cv2.morphologyEx(processed, cv2.MORPH_CLOSE, np.ones((10, 10), np.uint8))
+        dilatation_size = 15
+        # dilation_shape = cv2.MORPH_RECT
+        # dilation_shape = cv2.MORPH_ELLIPSE
+        dilation_shape = cv2.MORPH_CROSS
+        element = cv2.getStructuringElement(dilation_shape, (2 * dilatation_size + 1, 2 * dilatation_size + 1),
+                                    (dilatation_size, dilatation_size))
+        processed = cv2.dilate(processed, element)
+        processed = cv2.blur(processed, (8, 8))        
+        return processed
+
+    def detect_contours(self, frame, minimum_size):
+        """Returns contours larger then a specified size in a frame."""
+        contours, hierarchy = cv2.findContours(frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        large_contours = []
+        if len(contours) != 0:
+            for i in contours:
+                if ud_mouse.Check(cv2.contourArea(i), minimum_size):
+                   if cv2.contourArea(i) > minimum_size:
+                       large_contours.append(i)
+        return large_contours
+
+    def scale_contour(self,cnt, scale:float):
+        M = cv2.moments(cnt)
+        x = int(M['m10']/M['m00'])
+        y = int(M['m01']/M['m00'])
+
+        center = cnt - [x, y]
+        cnt_scaled = center * scale
+        cnt_scaled = cnt_scaled + [x, y]
+        cnt_scaled = cnt_scaled.astype(np.int32)
+
+        return cnt_scaled
+
+    def on_target(self, contour, hitbox):
+        for c in contour:
+            cont = self.scale_contour(c, hitbox)
+            test = cv2.pointPolygonTest(cont,( self.box_middle, self.box_middle),False)
+            if test >= 0:
+                return True
+        return False
+
+    def compute_centroid(self, contour):
+        """Returns x- and y- coordinates of the center of the largest contour."""
+        self.contour = contour
+        c = max(contour, key=cv2.contourArea)
+        rectangle = np.int0(cv2.boxPoints(cv2.minAreaRect(c)))
+        new_box = []
+        for point in rectangle:
+            point_x = point[0]
+            point_y = point[1]
+            new_box.append([round(point_x, -1), round(point_y, -1)])
+        M = cv2.moments(np.array(new_box))
+        if M['m00']:
+            center_x = (M['m10'] / M['m00'])
+            center_y = (M['m01'] / M['m00'])
+            x = -(self.box_middle - center_x)
+            y = -(self.box_middle - center_y)
+            self.cx = x
+            self.cy = y
+            return [], x, y
+
+
+
+    def is_activated(self, key_code) -> bool:
+        return ud_mouse.is_activated(key_code)
+
+    def flick_mouse(self, x, y):
+        threading.Thread(target=self._move_mouse, args=[x, y, self.x_multiplier*self.flick_speed, self.y_multiplier*(self.flick_speed/2), self.y_difference]).start()
+
+    def move_mouse(self, x, y):
+       threading.Thread(target=self._move_mouse, args=[x, y, self.x_multiplier, self.y_multiplier, self.y_difference]).start()
+
+    def _move_mouse(self, x, y, x_multiplier, y_multiplier, y_difference):
+        ud_mouse.move_mouse(x, y, self.box_size, x_multiplier, y_multiplier, y_difference)
+
+    def shift(self):
+        threading.Thread(target= self._shift).start()
+
+    def _shift(self):
+        ud_mouse.simulate_shift()
+
+    def Q(self):
+        threading.Thread(target= self._Q).start()
+
+    def _Q(self):
+        ud_mouse.simulate_Q()
+
+    def E(self):
+        threading.Thread(target= self._E).start()
+
+    def _E(self):
+        ud_mouse.simulate_E()
+
+    def mouse_right(self):
+        threading.Thread(target= self._mouse_right).start()
+
+    def _mouse_right(self):
+        ud_mouse.rclick_mouse()        
+
+    def click(self):
+        threading.Thread(target=self._click).start()
+
+    def trigger(self):
+        threading.Thread(target=self._click).start()
+        time.sleep(0.3)
+
+    def _click(self):        
+        ud_mouse.click_mouse()
+    
+
+
---
+++ readme.md
@@ -0,0 +1,49 @@
+Features added:
+
+* triggerbot + magnet.
+
+* abilities triggerbot + magnet.
+
+* shitty flick bot.
+
+* a much faster screengrab.
+
+* slightly better(?) smooth.
+
+
+
+I have made changes to the dll so to use this you' ll have to recompile it from the source uploaded here plus having: 
+
+* InputInterceptor.dll (https://github.com/0x2E757/InputInterceptor) 
+
+* interception.dll (https://github.com/0x2E757/InputInterceptor YOU ALSO NEED TO INSTALL THIS)
+
+* ClassLibrary1.dll
+
+All in the same folder. 
+
+
+
+
+
+
+Once thats done edit grabber.py line 14 to the location of the ClassLibrary dll.
+
+All the settings are in main.py but I dont really use anything else other then the aim key so 
+
+the settings arent tuned well, try messing around with the sleeps and stuff till you find a good balance.
+
+
+
+
+I dont really code/ know what I am doing and the code is incredibly lazy but still this works fairly well imo.
+
+
+
+Credits: 
+
+https://www.unknowncheats.me/forum/members/3853446.html (what this is pasted from)
+
+https://www.unknowncheats.me/forum/valorant/414206-implementing-speed-color-aimbot.html (part of the smooth)
+
+
